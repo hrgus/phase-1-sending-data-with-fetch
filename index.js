f01044cfa88f11208bc84faf8db388ff06cd1808
Harry <hrgus3@gmail.com>
2021-11-30 12:01:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your code here
+function submitData(name, email){
+   
+const dataObject = { 
+   method: "POST",
+   headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+   },
+   body: JSON.stringify({
+       name,
+       email
+   }),
+};
+
+return fetch("http://localhost:3000/users", dataObject)
+   .then(resp => resp.json())
+   .then(object => document.body.innerHTML = object.id)
+   .catch(function(error) {
+     document.body.innerHTML = error;
+   });
+
+}
+
+// const submitData = {
+//    method: "POST",
+//    headers: {
+//       "Content-type": "appliction/json",
+//    Accept: "application/json",
+//    },
+//    body: JSON.stringify({
+//       "name": "Harry",
+//       "email": "hrgus3@gmail.com",
+//    }),
+   
+//    return :theFetch
+// };
+
+// function theFetch (){
+// fetch("http://localhost:3000/users")
+//    .then(resp => resp.json())
+//    .then(data => console.log(data))
+// };
